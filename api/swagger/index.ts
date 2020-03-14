6e822837cfd128c016b34a981859c08cbfc4590d
Brian <bdombro@gmail.com>
2020-03-14 19:37:20 -0400

    Tweak

--- api/swagger/index.ts
+++ api/swagger/index.ts
@@ -5,11 +5,11 @@ import passwordProtected from 'password-protected';
 const repo = require('../../package.json').repository.url.slice(4);
 
 const app = express();
+app.use(shrinkRay());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
-app.use(shrinkRay());
 app.use(passwordProtected({
-    hint: `Hint: password is 'superpassword'. Source code for this app is at <a target='_blank' href='${repo}'>${repo}</a>`,
+    hint: `Hint: password is 'superpassword'. Source code for this app is in <a target='_blank' href='${repo}'>github</a>.`,
 }));
 app.use(express.static(__dirname + "/private")); // include any other files dist directory
 app.listen(3000, () => {
